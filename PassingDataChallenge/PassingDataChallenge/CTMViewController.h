35c06a7ff390c06e5ae1bb12cc62cb337552203f
Charles McQuilkin <chamcqui@usausrad2350.main.corp>
2014-10-10 13:21:28 -0500

    Finished Passing Data Challenge

---
+++ PassingDataChallenge/PassingDataChallenge/CTMViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTMViewController.h
+//  PassingDataChallenge
+//
+//  Created by Charles McQuilkin on 10/10/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTMViewController : UIViewController
+
+@end
---
+++ PassingDataChallenge/PassingDataChallenge/CTMViewController.m
@@ -0,0 +1,37 @@
+//
+//  CTMViewController.m
+//  PassingDataChallenge
+//
+//  Created by Charles McQuilkin on 10/10/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import "CTMViewController.h"
+
+@interface CTMViewController ()
+
+@end
+
+@implementation CTMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
