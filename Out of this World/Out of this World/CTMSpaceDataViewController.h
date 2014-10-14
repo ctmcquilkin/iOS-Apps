8f7dc8f6f8511def9fa98dc7723b19eee841bd66
Charles McQuilkin <chamcqui@usausrad2350.main.corp>
2014-10-14 17:32:06 -0500

    View Controller Edits

---
+++ Out of this World/Out of this World/CTMSpaceDataViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTMSpaceDataViewController.h
+//  Out of this World
+//
+//  Created by Charles McQuilkin on 10/10/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTMSpaceDataViewController : UIViewController
+
+@end
---
+++ Out of this World/Out of this World/CTMSpaceDataViewController.m
@@ -0,0 +1,37 @@
+//
+//  CTMSpaceDataViewController.m
+//  Out of this World
+//
+//  Created by Charles McQuilkin on 10/10/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import "CTMSpaceDataViewController.h"
+
+@interface CTMSpaceDataViewController ()
+
+@end
+
+@implementation CTMSpaceDataViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
