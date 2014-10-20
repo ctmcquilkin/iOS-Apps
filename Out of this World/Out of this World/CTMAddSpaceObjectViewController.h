e8fe479ad51d92f2994cc1f8e063d97c744243fc
Charles McQuilkin <chamcqui@usausrad2350.main.corp>
2014-10-20 11:25:37 -0500

    Incorporated Add button functionality

---
+++ Out of this World/Out of this World/CTMAddSpaceObjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTMAddSpaceObjectViewController.h
+//  Out of this World
+//
+//  Created by Charles McQuilkin on 10/20/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTMAddSpaceObjectViewController : UIViewController
+
+@end
