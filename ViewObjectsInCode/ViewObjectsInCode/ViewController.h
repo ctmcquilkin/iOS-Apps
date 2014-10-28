f6db91f7a792261f5ffa8bc5eb76d99ce3aca8f7
Charles McQuilkin <ctmcquilkin@gmail.com>
2014-10-28 11:42:11 -0500

    Finished View Objects in Code Project

---
+++ ViewObjectsInCode/ViewObjectsInCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewObjectsInCode
+//
+//  Created by Charles McQuilkin on 10/28/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
