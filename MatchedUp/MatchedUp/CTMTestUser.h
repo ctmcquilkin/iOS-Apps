685520c996852df339881e92609c093be90e5bf6
Charles McQuilkin <ctmcquilkin@gmail.com>
2014-11-21 17:04:59 -0600

    Added Test user, hooked up sliders, added saving tagline and logging out

---
+++ MatchedUp/MatchedUp/CTMTestUser.h
@@ -0,0 +1,13 @@
+//
+//  CTMTestUser.h
+//  MatchedUp
+//
+//  Created by Charles McQuilkin on 11/20/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTMTestUser : NSObject
+
+@end
---
+++ MatchedUp/MatchedUp/CTMTestUser.m
@@ -0,0 +1,13 @@
+//
+//  CTMTestUser.m
+//  MatchedUp
+//
+//  Created by Charles McQuilkin on 11/20/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import "CTMTestUser.h"
+
+@implementation CTMTestUser
+
+@end
