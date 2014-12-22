2db7399bdd8cd5d81e5d7a6afcbeebaeb3a5e38c
Charles McQuilkin <ctmcquilkin@gmail.com>
2014-12-22 09:14:15 -0600

    added list directions + snapshots

---
+++ WorldTraveler/WorldTraveler/TCDirectionsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCDirectionsListViewController.h
+//  WorldTraveler
+//
+//  Created by Charles McQuilkin on 12/22/14.
+//  Copyright (c) 2014 Charles McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCDirectionsListViewController : UIViewController
+
+@end
